88237b443435678690d848face8a57089e1ba3bc
lolenlen <152620064+lolenlen@users.noreply.github.com>
2024-10-03 17:23:47 +0200

    Update Week3.ino

--- Week3.ino
+++ Week3.ino
@@ -1,10 +1,23 @@
-const int analogInPinPM = A0; // Potentiometer
-const int analogInPinLDR = A1; // LDR
-const int analogOutPin = 13;   // Built-in LED
-const int buttonPin = 7;       // Button pin (changed to 7)
+const int analogInPinPM_1 = A0; // Potentiometer 1 PC0
+const int analogInPinPM_2 = A1; // PM 2 PC1
+                                // Connect both of PMs VCC to the + of breadboard (same with GND)
+                                // connected to the 3.3V/GND (NOT 5V it will smoke again) of atpmega
+const int analogOutPin = 13;    // Built-in LED
+const int buttonPin = 7;        // Button connect to PD7
 
-int PotentiometerVal = 0;
-int LDRVal = 0;
+int PM_Val_1 = 0;
+int PM_Val_2 = 0;
+int ButtonVal = HIGH;     
+
+/// LED fast blinking
+bool fastBlinking = false;      // Track fast blinking state
+unsigned long previousMillis = 0; // Store last time LED was updated
+int blinkInterval = 500;        // Default to 1Hz (500ms on/off)
+
+// Button debouncing
+const unsigned long debounceDelay = 50; // milliseconds
+unsigned long lastDebounceTime = 0; // last time the button state changed
+int lastButtonState = HIGH; // previous button state
 
 void setup() {
   Serial.begin(9600);
@@ -12,33 +25,60 @@ void setup() {
   Serial.println("2153058");
   Serial.println("Week 3. Analog");
 
-  // Set LED pin
   pinMode(analogOutPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);  // Use pin 7 and internal pull-up resistor
+  pinMode(buttonPin, INPUT_PULLUP);  
 }
 
 void loop() {
-  PotentiometerVal = analogRead(analogInPinPM);
-  LDRVal = analogRead(analogInPinLDR);
-  int ButtonVal = digitalRead(buttonPin);
+  // Read PMs values
+  PM_Val_1 = analogRead(analogInPinPM_1);
+  PM_Val_2 = analogRead(analogInPinPM_2);
+  
+  // Read button
+  int reading = digitalRead(buttonPin);
 
-  // LED control based on potentiometer and LDR values
-  if (PotentiometerVal > 75 && LDRVal > 75) {
-    digitalWrite(analogOutPin, HIGH);  // Turn on LED if conditions are met
-
-    // Check if button is pressed (input will be LOW when pressed)
-    if (ButtonVal == LOW) {
-      digitalWrite(analogOutPin, LOW);  // Turn off LED if button is pressed
-    }
-  } else {
-    digitalWrite(analogOutPin, LOW);  // Turn off LED if conditions aren't met
+  // Check button state
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis(); // reset debounce timer
   }
 
-  // Print sensor values to Serial Monitor
-  Serial.print("sensor PM = ");
-  Serial.print(PotentiometerVal);
-  Serial.print("\t sensor LDR = ");
-  Serial.println(LDRVal);
+  // Only change button state if button state has been stable
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != ButtonVal) {
+      ButtonVal = reading; // save button state
+    }
+  }
+  lastButtonState = reading; // save last state
 
-  delay(500);  // Longer delay for easier reading
+  // Check if button is pressed 
+  if (ButtonVal == LOW) {
+    // if pressed, revert to 1Hz blinking
+    fastBlinking = false;
+    blinkInterval = 500;  // 1Hz (500ms on, 500ms off)
+  } else {
+    // If PM values greater than 75, set 10Hz blinking
+    if (PM_Val_1 > 75 && PM_Val_2 > 75) {
+      fastBlinking = true;
+      blinkInterval = 50; // 10Hz (50ms on, 50ms off)
+    } else {
+      // else maintain 1Hz blinking
+      fastBlinking = false;
+      blinkInterval = 500;  // 1Hz (500ms on, 500ms off)
+    }
+  }
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= blinkInterval) {
+    previousMillis = currentMillis; 
+    digitalWrite(analogOutPin, !digitalRead(analogOutPin));
+  }
+
+  Serial.print(" sensor left = ");
+  Serial.print(PM_Val_1);
+  Serial.print(" sensor right = ");
+  Serial.print(PM_Val_2);
+  Serial.print(" Button = ");
+  Serial.println(ButtonVal);
+  
 }
